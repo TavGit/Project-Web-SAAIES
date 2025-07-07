4439fce4c530b193d122710c3e15ad5f2e74e7c1
TavGit <gustavbaim18@gmail.com>
2025-07-08 00:00:57 +0700

    update

--- script.js
+++ script.js
@@ -187,3 +187,26 @@ function type() {
 }
 
 type(); // memulai animasi typing saat halaman dibuka
+
+// Logika Swiper Javascript
+const swiper = new Swiper(".fasilitas-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});

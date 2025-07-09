f885d060a4512cf1878f93e0dce7d863427562f4
TavGit <gustavbaim18@gmail.com>
2025-07-09 15:50:57 +0700

    update

---
+++ javascript/sd.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Logika Swiper Javascript
+  const swiper = new Swiper(".banner-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});

433426ad453ac00cb26fe45f99efd8778f910ee0
TavGit <gustavbaim18@gmail.com>
2025-07-09 21:13:54 +0700

    update

--- javascript/news.js
+++ javascript/news.js
@@ -38,3 +38,18 @@ function showNewsListing() {
 document.addEventListener("DOMContentLoaded", () => {
   showNewsListing(); // panggil function yg bener buat nampilin daftar news utama
 });
+
+// kita buat logika utama pada saat halaman news.html ini dipanggil dari halaman akademik,sd,smp,sma
+document.addEventListener("DOMContentLoaded", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const newsId = urlParams.get("newsId"); // Ambil nilai parameter 'newsId' dari URL
+
+  if (newsId) {
+    // Jika ada newsId di URL, panggil showNews() untuk menampilkan berita spesifik tersebut
+    showNews(newsId);
+  } else {
+    // Jika tidak ada newsId di URL (misalnya user langsung akses news.html tanpa parameter),
+    // tampilkan daftar berita utama
+    showNewsListing();
+  }
+});
